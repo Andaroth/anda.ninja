2da16674721ab01a9defa476810d31443dce684e
Anda <ax.fiolle@gmail.com>
2024-09-05 21:40:19 +0200

    Remake

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         {children}
-        <ACursor />
+        {/* <ACursor /> */}
         </body>
     </html>
   );
