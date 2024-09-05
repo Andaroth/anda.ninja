47256b467e30e8c4236b4ea1f759ddf2d4a45720
Anda <ax.fiolle@gmail.com>
2024-09-05 21:55:22 +0200

    Update og meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,32 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const title = "Anda Ninja"
+  const description = "Creator of incredible interfaces to make the web a wonderful world."
+
   return (
     <html lang="en">
       <head>
-        <title>Anda Pirate vCard</title>
-        <meta name="description" content="List of links of Anda Pirate" />
+        <title>{title || "Andaroth"}</title>
+        <link rel="icon" href="./img/fav.gif" />
+        <meta property="og:image" content="./img/fav.gif" />
+        <meta property="og:title" content={title || "Andaroth"} />
+        <meta property="og:description" content={description} />
+        <meta name="description" content={description} />
+
+        <meta
+          name="keywords"
+          content="axel,fiolle,andaroth,developper,coder,blockchain,ethereum,crypto,btc,eth,web,designer,user,experience,interface,ui,ux,uxui,responsive,js,javascript,css,html,php,mysql,postgres,sql,node,nodejs,solidity"
+        />
+
+        <meta name="author" content="Axel Fiolle" />
+
+        <meta name="msapplication-tooltip" content={description} />
+        <meta name="msapplication-starturl" content="https://anda.ninja/" />
+
+        <meta name="wot-verification" content="b6e6901e016c39508cad" />
       </head>
+
       <body className={inter.className}>
         {children}
         {/* <ACursor /> */}
